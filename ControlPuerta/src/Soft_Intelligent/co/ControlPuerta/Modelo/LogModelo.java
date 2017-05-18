e6861657e7f74512157d4350b78ea6358e78978a
ESTUDIANTE1201T <ESTUDIANTE1201T@S1201PC9.mipana.int>
2017-05-17 21:22:38 -0500

    Merge origin/master
    Conflicts:
    	ControlPuerta/src/Soft_Intelligent/co/ControlPuerta/Vista/EstadosGenerales.java
    	ControlPuerta/src/Soft_Intelligent/co/ControlPuerta/Vista/Log.java

--- ControlPuerta/src/Soft_Intelligent/co/ControlPuerta/Modelo/LogModelo.java
+++ ControlPuerta/src/Soft_Intelligent/co/ControlPuerta/Modelo/LogModelo.java
@@ -24,28 +24,27 @@ public class LogModelo {
     
     private final String tabla = "Dispositivo";
 
-    public void Crear(Connection Conexion, LogModeControl Log) throws SQLException {
-        try {
-            PreparedStatement Guardar = null;
-
-            Guardar = Conexion.prepareStatement("INSERT INTO " + this.tabla + "( \"Puerta\", \"IP\", \"Hora Inicial\", \"Hora Final\", \"Estado\", \"Ubicacion\", \"Nombre\") VALUES(?,?,?,?,?,?,?)");
-            Guardar.setString(1, Log.getPuerta());
-            Guardar.setString(2, Log.getIP());
-            Guardar.setString(3, Log.getHi());
-            Guardar.setString(4, Log.getHf());
-            Guardar.setString(5, Log.getEstado());
-            Guardar.setString(6, Log.getUbicacion());
-            Guardar.setString(7, Log.getNombre());
-            Guardar.setInt(8, Log.getID_Estado());
-            
-            
-
-            Guardar.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new SQLException(e);
-        }
-    }
+//    public void Crear(Connection Conexion, LogModeControl Log) throws SQLException {
+//        try {
+//            PreparedStatement Guardar = null;
+//
+//            Guardar = Conexion.prepareStatement("INSERT INTO " + this.tabla + "( Puerta, IP, Hora Inicial, Hora Final, Estado, Ubicacion, Nombre) VALUES(?,?,?,?,?,?,?,?)");
+//            Guardar.setString(1, Log.getPuerta());
+//            Guardar.setString(2, Log.getIP());
+//            Guardar.setString(3, Log.getHi());
+//            Guardar.setString(4, Log.getHf());
+//            Guardar.setString(5, Log.getEstado());
+//            Guardar.setString(6, Log.getUbicacion());
+//            Guardar.setString(7, Log.getNombre());
+//            Guardar.setInt(8, Log.getID_Estado());
+//                    
+//            Guardar.executeUpdate();
+//            System.out.println("Registro Adicionado");
+//
+//        } catch (SQLException e) {
+//            throw new SQLException(e);
+//        }
+//    }
     public ArrayList<LogModeControl> Logactividad() {
 
         ConectMySql conex = new ConectMySql();
@@ -54,6 +53,12 @@ public class LogModelo {
         try {
 
             Statement estatuto = conex.conexion().createStatement();
+            System.out.println("Select Dis.Nombre,Dis.IP,Ala.HoraIni,Ala.HoraFin,Est.Nombre As Estado,Acc.nombre As Accion,Usu.Nombre As Usuario from Evento Eve\n" +
+"inner join alarma Ala on  eve.ID_Alarm=Ala.ID\n" +
+"inner join Dispositivo Dis on  eve.ID_Disp=Dis.ID\n" +
+"inner join Accion Acc on  eve.ID_Acci=Acc.ID\n" +
+"inner join Usuario Usu on  eve.Cedula=Usu.Cedula\n" +
+"inner join Estado Est on  Est.ID=Dis.ID_Estado");
             ResultSet rs = estatuto.executeQuery("Select Dis.Nombre,Dis.IP,Ala.HoraIni,Ala.HoraFin,Est.Nombre As Estado,Acc.nombre As Accion,Usu.Nombre As Usuario from Evento Eve\n" +
 "inner join alarma Ala on  eve.ID_Alarm=Ala.ID\n" +
 "inner join Dispositivo Dis on  eve.ID_Disp=Dis.ID\n" +
@@ -63,15 +68,12 @@ public class LogModelo {
 
             while (rs.next()) {
                 Puerta = new LogModeControl();
-                Puerta.setID(Integer.parseInt(rs.getString("ID")));
-                Puerta.setPuerta(rs.getString("Puerta"));
-                Puerta.setUbicacion(rs.getString("Ubicacion"));
+                Puerta.setNombre(rs.getString("Nombre"));
                 Puerta.setIP(rs.getString("IP"));
+                Puerta.setHoraIni(rs.getString("HoraIni"));
+                Puerta.setHoraFin(rs.getString("HoraFin"));
+                Puerta.setUsuario(rs.getString("Usuario"));
                 Puerta.setEstado(rs.getString("Estado"));
-                Puerta.setEstado(rs.getString("Hora inicial"));
-                Puerta.setEstado(rs.getString("Hora Final"));
-                Puerta.setEstado(rs.getString("Nombre Usuario"));
-                Puerta.setEstado(rs.getString("Ubicacion"));
                 Lista.add(Puerta);
             }
             rs.close();
@@ -85,11 +87,6 @@ public class LogModelo {
 
         }
         return Lista;
-    }
-
-    public void setVisible(boolean b) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+    } 
     
 }
